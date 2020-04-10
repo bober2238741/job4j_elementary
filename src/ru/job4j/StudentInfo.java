bea2aecc74846300c6287970c32a29909c6c18ee
Bober2761454 <Bober276@gmail.com>
2020-04-10 21:34:52 +0600

    3.3. Разбор первой программы.[#271162]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Andrey Klevchenkov");
+        System.out.println("17.06.1993");
+    }
+}
