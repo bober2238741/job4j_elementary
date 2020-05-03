c2606ba80c01b8460bac0c38921fc899e89a8389
bober2238741 <Bober276@gmail.com>
2020-05-03 17:26:37 +0600

    6.0.5. Массивы в тестах. [#271133]

--- src/ru/job4j/array/Square.java
+++ src/ru/job4j/array/Square.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class Square {
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
-        for (int i = 0; i < bound; i++) {
-            rst[i] = (int) Math.pow(i, 2);
+        for (int i = 1; i < bound; i++) {
+            rst[i] = i * i;
         }
         return rst;
     }
