993db8cc3d6e32c31b29ff71e82a42b6e5ce4d6c
bober2238741 <Bober276@gmail.com>
2020-10-30 14:22:30 +0600

    6.4. Слово начинается с ... [#271119]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+
+        if (pref.length > word.length || pref.length == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
